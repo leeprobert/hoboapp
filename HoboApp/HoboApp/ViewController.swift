ab425522fc091e70d0f14cb95f418b7a0dbbafa1
Lee Probert <leeprobert@gmail.com>
2015-10-26 16:24:41 +0000

    Boilerplate Xcode project

---
+++ HoboApp/HoboApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HoboApp
+//
+//  Created by WLD_MBP_20 on 26/10/2015.
+//  Copyright (c) 2015 probert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
